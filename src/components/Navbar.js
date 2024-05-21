ebcf1d506779095489970edc84c0237a2603ccb0
brightnc <bright.siwat@hotmail.com>
2024-05-21 13:08:09 +0700

    init project

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-indigo-600">
+      <div className="logo text-5xl font-light">
+        Live<span className="font-bold">Eleven</span>
+      </div>
+      <div className="links"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { Noto_Sans_Thai } from "next/font/google";
+
+const notoSansThai = Noto_Sans_Thai({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSansThai.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
