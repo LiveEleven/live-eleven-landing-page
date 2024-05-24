82853029d2371a2c3e5e5764866da186bccc2db8
brightnc <bright.siwat@hotmail.com>
2024-05-24 13:32:26 +0700

    tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-color": "#A62020",
+        "secondary-color": "#D5D5D5",
+      },
     },
   },
   plugins: [],
 };
+
