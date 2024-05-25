ac15b8440e2e016142a843b35d0aea66bcc1aea4
brightnc <bright.siwat@hotmail.com>
2024-05-25 13:37:54 +0700

    fix: style drop shadow and add duration button

---
+++ src/components/LiveService.js
@@ -0,0 +1,5 @@
+const LiveService = () => {
+  return <div>LiveService</div>;
+};
+
+export default LiveService;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import LiveService from "@/components/LiveService";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      {/* <LiveService /> */}
     </main>
   );
 }
