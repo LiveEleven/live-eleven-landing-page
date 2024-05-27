1aba9533719e6400e3d87129962dfc278bd6ae9c
Peet <werapatsaejong@gmail.com>
2024-05-27 11:46:27 +0700

    components/LiveStream

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Header from "@/components/Header";
 import LiveService from "@/components/LiveService";
+import LiveStream from "@/components/LiveStream";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <LiveStream />
       {/* <LiveService /> */}
     </main>
   );
 }
-
