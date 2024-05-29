340b7070d71ee59aa6a6b493c7d49a877f62d271
Peet <werapatsaejong@gmail.com>
2024-05-29 21:03:01 +0700

    components/SetUpLiveStreaming

---
+++ src/components/SetUpLiveStreaming.js
@@ -0,0 +1,107 @@
+const SetUpLiveStreaming = () => {
+  return (
+    <div className="bg-primary-color">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl  text-center text-white font-bold font-head">
+        SET UP LIVE STREAM
+      </h1>
+      <hr class="w-32 h-1 mx-auto my-6 bg-white border-0 rounded md:my-16 md:w-40 lg:w-48" />
+
+      <p className="font-body text-[10px] text-center text-white md:text-xl lg:text-3xl 2xl:text-4xl">
+        ให้บริการและคำแนะนำการ set ระบบ live และการใช้งานอุปกรณ์
+      </p>
+      <p className="font-body text-[10px] text-center text-white md:text-xl lg:text-3xl 2xl:text-4xl">
+        สำหรับคนที่ต้องการทำ live ด้วยตัวเอง หรืออยากเพิ่มคุณภาพการ live
+      </p>
+      <p className="font-body text-[10px] text-center text-white md:text-xl lg:text-3xl 2xl:text-4xl">
+        จากโทรศัพท์มือถือมาเป็นกล้อง เพื่อให้ได้ภาพที่คมชัดและสมูทมากยิ่งขึ้น
+      </p>
+      <p className="font-body text-[10px] text-center text-white md:text-xl lg:text-3xl 2xl:text-4xl">
+        แถมยังสามารถขึ้น Art work โชว์ราคาสินค้า โปรโมชั่น ให้เป็น professional
+        ได้อีกด้วย
+      </p>
+      <hr class="w-32 h-1 mx-auto mt-10 bg-white border-0 rounded md:mt-20 md:w-40 lg:w-48 mb-5" />
+
+      <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl  text-center text-white font-bold font-head my-20">
+        SET UP LIVE STREAM PACKAGES
+      </h2>
+      <div className="flex flex-col mx-0 lg:mx-72 2xl:mx-96 lg:flex-row">
+        <div className="bg-white flex flex-col items-center mx-auto px-5 border-2 border-primary-color rounded-lg mt-10 shadow-column lg:shadow-row">
+          <h2 className="font-head text-xl font-bold mt-6 md:text-3xl lg:text-2xl">
+            Package Live [S]
+          </h2>
+          <h3 className="font-head text-6xl font-bold my-12 md:text-8xl lg:text-7xl">
+            15,000 ฿
+          </h3>
+          <div>
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Mirrorless Camera
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Wireless Microphone
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Router Internet 5G
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Live System
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Final Monitor
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              Stream Duration 1 hr.
+            </p>
+          </div>
+          <p className="font-body text-center mt-56 mb-6 text-[9px] md:text-sm lg:text-xs">
+            *เป็นราคาเบื้องต้นอาจมีการปรับเปลี่ยนตามขอบเขตและรายละเอียดของงาน
+          </p>
+        </div>
+
+        <div className="bg-white flex flex-col items-center mx-auto px-5 border-2 border-primary-color rounded-lg mt-10 shadow-column lg:shadow-row">
+          <h2 className="font-head text-xl font-bold mt-6 md:text-3xl lg:text-2xl">
+            Package Live [S]
+          </h2>
+          <h3 className="font-head text-6xl font-bold my-12 md:text-8xl lg:text-7xl">
+            15,000 ฿
+          </h3>
+          <div>
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Mirrorless Camera
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Wireless Microphone
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Router Internet 5G
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Live System
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              1 Final Monitor
+            </p>
+            <hr class="w-60 h-0.5 mx-auto my-6 bg-secondary-color border-0 rounded md:my-10 md:w-80 lg:w-48" />
+            <p className="font-head text-lg text-center font-semibold md:text-2xl lg:text-lg">
+              Stream Duration 1 hr.
+            </p>
+          </div>
+          <p className="font-body text-center mt-56 mb-6 text-[9px] md:text-sm lg:text-xs">
+            *เป็นราคาเบื้องต้นอาจมีการปรับเปลี่ยนตามขอบเขตและรายละเอียดของงาน
+          </p>
+        </div>
+      </div>
+      <hr class="w-32 h-1 mx-auto mt-10 bg-white border-0 rounded md:mt-20 md:w-40 lg:w-48" />
+    </div>
+  );
+};
+
+export default SetUpLiveStreaming;
