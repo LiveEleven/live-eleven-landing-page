13f4945369d44261368ea9da831a93d80e4548fe
brightnc <bright.siwat@hotmail.com>
2024-05-29 16:48:58 +0700

    feat: record live component

---
+++ src/components/RecordLive.js
@@ -0,0 +1,26 @@
+const RecordLive = () => {
+  return (
+    <div className="bg-primary-color flex flex-col items-center text-white w-full md:h-[600px] lg:flex-row xl:h-[800px] lg:px-14 py-6">
+      <div className="left w-full h-1/2 lg:h-3/5 flex justify-center ">
+        <iframe
+          src="https://www.youtube.com/embed/Uj4AWaR7gjU"
+          className="w-4/5 lg:w-full h-full"
+        />
+      </div>
+      <div className="right w-full h-1/2 mt-6">
+        <h2 className="font-bold font-head text-center text-2xl md:text-5xl lg:text-6xl">
+          Record Live
+        </h2>
+        <p className="font-body text-xs text-white text-center md:text-3xl 2xl:text-4xl lg:text-left mt-4 px-9 lg:indent-14 ">
+          การบันทึกการถ่ายทอดสด (Record Live)
+          เป็นบริการที่ช่วยให้คุณเก็บทุกช่วงเวลาสำคัญ ในระหว่างการสตรีมมิ่ง
+          เพื่อให้คุณสามารถนำวีดีโอที่บันทึกไว้ไปใช้ประโยชน์ในภายหลังได้ตามต้องการ
+          ไม่ว่าจะเป็นการเผยแพร่ซ้ำ การแบ่งปันบนโซเชียลมีเดีย
+          หรือการวิเคราะห์และการปรับปรุงการสตรีมมิ่ง ครั้งถัดไป
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RecordLive;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import LivePackages from "@/components/LivePackages";
 import LiveService from "@/components/LiveService";
 import LiveStream from "@/components/LiveStream";
 import Navbar from "@/components/Navbar";
+import RecordLive from "@/components/RecordLive";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <LiveStream />
       <LiveService />
+      <RecordLive />
       <LivePackages />
       <EventOrganizer />
       <AboutUs />
