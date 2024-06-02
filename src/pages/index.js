e96a01c9d098bc47687bc6893dda3c3e5d8d7fc5
brightnc <bright.siwat@hotmail.com>
2024-06-02 13:47:36 +0700

    feat: equipment service component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import AboutUs from "@/components/AboutUs";
+import EquipmentService from "@/components/EquipmentService";
 import EventOrganizer from "@/components/EventOrganizer";
 import Header from "@/components/Header";
 import LivePackages from "@/components/LivePackages";
@@ -16,9 +17,11 @@ export default function Home() {
       <LiveService />
       <RecordLive />
       <LivePackages />
+      <EquipmentService />
       <EventOrganizer />
       <SetUpLiveStreaming />
       <AboutUs />
     </main>
   );
 }
+
