d2c3aac003951a8830e3ba3e4e2f25d965205f21
Peet <werapatsaejong@gmail.com>
2024-06-06 21:42:50 +0700

    fix height Carousel OurWork

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -12,7 +12,7 @@ const OurWork = () => {
         </h1>
       </div>
       <hr class="w-32 h-1 mx-auto mt-6 bg-primary-color border-0 rounded md:mt-16 md:w-40 lg:w-48" />
-      <div className="mx-auto h-[300px] w-[300px] md:h-[700px] md:w-[700px] lg:h-[1350px] lg:w-[1350px] 2xl:h-[1600px] 2xl:w-[1600px]">
+      <div className="mx-auto h-[250px] w-[300px] md:h-[500px] md:w-[700px] lg:h-[1050px] lg:w-[1350px] 2xl:h-[1200px] 2xl:w-[1600px]">
         <CustomCarousel>
           <div className="grid grid-cols-2 grid-rows-2 gap-5">
             <div>
