1cae0eee6e1ee775b80b56aa2a6f2f5b8ec45263
brightnc <107994239+brightnc@users.noreply.github.com>
2024-07-01 21:48:50 +0700

    Update AddOnsCol3.js

--- src/components/AddOnsCol3.js
+++ src/components/AddOnsCol3.js
@@ -4,6 +4,25 @@ import Image from "next/image";
 const AddOnsCol3 = () => {
   return (
     <div className="flex flex-col gap-4 md:hidden lg:flex">
+     <Menu>
+        <MenuButton className="flex items-center gap-3">
+          <Image
+            src="/plus.png"
+            width={50}
+            height={50}
+            alt="Plus"
+            className="w-4 h-4"
+          />
+          <p className="text-left font-head text-sm lg:text-lg">Off-Site</p>
+        </MenuButton>
+        <MenuItems>
+          <MenuItem>
+            <p className="text-sm bg-gray-200 py-1 px-3 w-fit rounded-md">
+              เริ่มต้น 2000 ฿
+            </p>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
       <Menu>
         <MenuButton className="flex items-center gap-3">
           <Image
